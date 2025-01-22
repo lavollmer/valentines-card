887032438072d86b4eb3f6cead151ee474685d1b
Laura <laura.e.vollmer@gmail.com>
2025-01-21 21:30:35 -0600

    made a generic app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import "./index.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <>
       <div className="bg-mauvePink h-screen flex justify-center items-center">
-        <h1>Happy Valentine's Day!</h1>
+        <motion.div
+          initial={{ opacity: 0, y: -400 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="w-80 h-80 bg-fairytalePink rounded-lg"
+        >
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1>Happy Valentine's Day!</h1>
+          </div>
+        </motion.div>
       </div>
     </>
   );
