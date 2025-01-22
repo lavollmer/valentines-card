46acc6bea23fc6ba8cba6d47f39d2cb25c2015fa
Laura <laura.e.vollmer@gmail.com>
2025-01-21 21:37:45 -0600

    updating the code with handleSubmit and form options

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import "./App.css";
 import "./index.css";
 import "./components/ValentinesCard";
-import { motion } from "framer-motion";
 import ValentinesCard from "./components/ValentinesCard";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(`Message: ${message} | Name: ${name}`);
+  }
+
   return (
     <>
       <div className="bg-mauvePink h-screen flex justify-center items-center">
-        <ValentinesCard />
+        <form>
+          <textarea
+          value={name}
+          placeholder="Please enter your name"
+          onChange={(e) => setName(e.target.value)}
+          />
+          <textarea
+            value={message}
+            placeholder="Please enter a message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
       </div>
     </>
   );
