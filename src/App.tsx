185f1be55703d629c0879dc478f69234c66c459b
Laura <laura.e.vollmer@gmail.com>
2025-01-21 21:32:40 -0600

    created a generic template for valentines day card

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import "./App.css";
 import "./index.css";
+import "./components/ValentinesCard";
 import { motion } from "framer-motion";
+import ValentinesCard from "./components/ValentinesCard";
 
 function App() {
   return (
     <>
       <div className="bg-mauvePink h-screen flex justify-center items-center">
-        <motion.div
-          initial={{ opacity: 0, y: -400 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="w-80 h-80 bg-fairytalePink rounded-lg"
-        >
-          <div className="flex flex-col items-center justify-center h-full">
-            <h1>Happy Valentine's Day!</h1>
-          </div>
-        </motion.div>
+        <ValentinesCard />
       </div>
     </>
   );
---
+++ src/components/ValentinesCard.tsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+
+function ValentinesCard() {
+  return (
+    <>
+      <div className="bg-mauvePink h-screen flex justify-center items-center">
+        <motion.div
+          initial={{ opacity: 0, y: -400 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="w-80 h-80 bg-fairytalePink rounded-lg"
+        >
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1>Happy Valentine's Day!</h1>
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
+}
+
+export default ValentinesCard;
