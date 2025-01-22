be490e21edc650fa2127eca8cec50c8683f6ebde
Laura <laura.e.vollmer@gmail.com>
2025-01-21 21:15:26 -0600

    added Google Fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      colors: {
+        fairytalePink: "F1C8DB",
+        mauvePink: "B88C9E",
+        eggplant: "704C5E",
+        greenCadet: "88A09E",
+        greenV: "558B6E",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
