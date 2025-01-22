bf1b353819582b9a46bbbba13bb89eadc0c95f72
Laura <laura.e.vollmer@gmail.com>
2025-01-21 21:57:01 -0600

    adding styling to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import "./index.css";
 import "./components/ValentinesCard";
+import Footer from "./components/Footer";
 import React, { useState } from "react";
+import { CiHeart } from "react-icons/ci";
 
 function App() {
   const [message, setMessage] = useState();
@@ -16,34 +18,43 @@ function App() {
     <>
       <div className="bg-mauvePink h-screen flex flex-col justify-center items-center font-rubik">
         <div>
-          <h1 className="font-bold text-xl p-10">Send a Valentine's Card</h1>
+          <h1 className="font-bold text-xl p-10">
+            Send a Valentine's Card
+            <CiHeart className="inline-block text-2xl text-fairytalePink" />
+          </h1>
         </div>
-        <div className="w-80 h-80 bg-fairytalePink rounded-lg">
+        <div className="w-80 h-80 bg-fairytalePink rounded-lg item-stretch">
           <form
             className="flex flex-col items-center justify-center space-y-8 h-full w-full"
             onSubmit={handleSubmit}
           >
-            <div>
+            <div className="flex flex-col justify-start space-y-4">
               <h1>What is their name?</h1>
               <input
                 value={name}
-                placeholder="Please enter your name"
+                placeholder=" Please enter your name"
                 onChange={(e) => setName(e.target.value)}
+                className="h-10 border border-eggplant rounded-lg"
               />
             </div>
-            <div>
+            <div className="flex flex-col justify-start space-y-4">
               <h1>What is your message?</h1>
               <input
                 value={message}
-                placeholder="Please enter a message"
+                placeholder=" Please enter a message"
+                className="h-10 row-span-4 border border-eggplant rounded-lg"
                 onChange={(e) => setMessage(e.target.value)}
               />
             </div>
-            <button className="px-8 py-2 bg-eggplant rounded-lg" type="submit">
+            <button
+              className="px-8 py-2 bg-eggplant rounded-lg text-white hover:bg-greenCadet"
+              type="submit"
+            >
               Send
             </button>
           </form>
         </div>
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col pt-10">
+      <p className="font-rubik text-xs">Created by Laura Dev 2025. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
