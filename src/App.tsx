0202e608a775dba81d1227b89352bc72bbbfb954
Laura <laura.e.vollmer@gmail.com>
2025-01-21 21:22:56 -0600

    debugging tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
-import './index.css'
+import "./App.css";
+import "./index.css";
 
 function App() {
-
   return (
     <>
-      <h1>Happy Valentine's Day!</h1>
+      <div className="bg-mauvePink h-screen flex justify-center items-center">
+        <h1>Happy Valentine's Day!</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
