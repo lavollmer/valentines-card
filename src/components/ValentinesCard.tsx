a954994623e6c9e4ba390f2890e3b82b645c37f5
Laura <laura.e.vollmer@gmail.com>
2025-01-24 14:15:01 -0600

    styling the Valentines Day Card

--- src/components/ValentinesCard.tsx
+++ src/components/ValentinesCard.tsx
@@ -6,28 +6,26 @@ interface Props {
   fromName: string;
 }
 
-const ValentinesCard:React.FC<Props> = ({ firstName, message,fromName }) => {
+const ValentinesCard: React.FC<Props> = ({ firstName, message, fromName }) => {
   return (
     <>
       <div className="bg-mauvePink h-screen flex justify-center items-center">
         <motion.div
           initial={{ opacity: 0, y: -400 }}
           animate={{ opacity: 1, y: 0 }}
-          className="w-80 h-80 bg-fairytalePink rounded-lg"
+          className="w-80 h-80 bg-cherryRed rounded-lg"
         >
-          <div className="flex flex-col items-center justify-center h-full">
-            <h1>Happy Valentine's Day {firstName}!</h1>
-          </div>
-          <div>
-            {message}
-          </div>
-          <div>
-            From {fromName}
+          <div className="flex flex-col items-center justify-center h-full font-rubik text-white">
+            <div>
+              <h1>Happy Valentine's Day {firstName}!</h1>
+            </div>
+            <div>{message}</div>
+            <div>From {fromName}</div>
           </div>
         </motion.div>
       </div>
     </>
   );
-}
+};
 
 export default ValentinesCard;
