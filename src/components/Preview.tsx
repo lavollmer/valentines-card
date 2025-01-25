8932fcacc18bea94d4221f9dba3fa3db99d82399
Laura <laura.e.vollmer@gmail.com>
2025-01-25 10:28:19 -0600

    working on card preview section

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -12,34 +12,40 @@ interface Props {
 const Preview: React.FC<Props> = () => {
   const location = useLocation();
   const { firstName, message, fromName } = location.state as Props;
-  
+
   const returnHome = () => {
     window.location.href = "/";
   };
 
   return (
     <div className="bg-mauvePink h-screen w-screen flex flex-col justify-center items-center">
-      <div className="flex flex-row items-center justify-center h-full">
+      <div className="flex flex-row items-center justify-between h-full">
         <div>
-        <YouAreLoved
-          firstName={firstName}
-          message={message}
-          fromName={fromName}
-        />
-        <Vintage firstName={firstName} message={message} fromName={fromName} />
-        <ValentinesCard
-          firstName={firstName}
-          message={message}
-          fromName={fromName}
-        />
+          <YouAreLoved
+            firstName={firstName}
+            message={message}
+            fromName={fromName}
+          />
+          <Vintage
+            firstName={firstName}
+            message={message}
+            fromName={fromName}
+          />
+          <ValentinesCard
+            firstName={firstName}
+            message={message}
+            fromName={fromName}
+          />
         </div>
-        <button
-          type="button"
-          onClick={returnHome}
-          className="px-8 py-2 bg-greenCadet text-white rounded-lg font-rubik"
-        >
-          Return to Home
-        </button>
+        <div>
+          <button
+            type="button"
+            onClick={returnHome}
+            className="px-8 py-2 bg-greenCadet text-white rounded-lg font-rubik"
+          >
+            Return to Home
+          </button>
+        </div>
       </div>
     </div>
   );
