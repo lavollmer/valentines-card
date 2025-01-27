51801775a5442eae50f3f7d03ab7ccdea783ffa0
Laura <laura.e.vollmer@gmail.com>
2025-01-27 14:27:26 -0600

    installed react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import "../index.css";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col text-white">
       <p className="font-rubik text-xs">Created by Laura Dev 2025. All rights reserved.</p>
     </div>
   );
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,11 +1,15 @@
 import "../App.css";
 import "../index.css";
 import Footer from "./Footer";
+import Navigation from "./Navigation";
 import CardPink from "../assets/combinedcardpink-removebg-preview.png";
 
 const LandingPage = () => {
   return (
     <div className="bg-bubblePink w-screen h-screen flex flex-col justify-between items-center p-10">
+      <div>
+        <Navigation />
+      </div>
       <div className="flex grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="font-rubik text-white text-4xl flex flex-col items-center justify-center">
           <h1>SEND A VALENTINES CARD</h1>
