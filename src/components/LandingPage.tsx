dc0d682ffd094e07d01af793af2cddf734bb7a9f
Laura <laura.e.vollmer@gmail.com>
2025-01-28 12:28:49 -0600

    updating the Landing Page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -6,16 +6,16 @@ import CardPink from "../assets/combinedcardpink-removebg-preview.png";
 
 const LandingPage = () => {
   return (
-    <div className="bg-redred w-screen h-screen flex flex-col justify-between items-center p-10">
+    <div className="bg-bubblePink w-screen h-screen flex flex-col justify-between items-center p-10">
       <div>
         <Navigation />
       </div>
       <div className="flex grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="font-rubik text-white flex flex-col items-center justify-center">
-          <h1 className="text-4xl bg-gradient-to-r from-blue-600 to-blue-800 inline-block font-bold text-transparent bg-clip-text">
-            Send a beautiful Valentine's Digital Card
+        <div className="font-rubik text-white flex flex-col items-center justify-center space-y-6">
+          <h1 className="text-4xl bg-gradient-to-r from-navyBlue to-blue-800 inline-block font-bold text-transparent bg-clip-text">
+            It's never been easier to send a Valentine's Day card
           </h1>
-          <p className="text-md text-center">
+          <p className="text-md text-left">
             Looking for the perfect way to show your love this Valentine's Day?
             Send a personalized digital card that speaks from the heart! Our
             collection of beautiful, customizable e-cards lets you express your
@@ -24,7 +24,7 @@ const LandingPage = () => {
             your message unforgettable. Quick, easy, and delivered instantly –
             send a little love today!
           </p>
-          <button className="py-2 px-4 rounded-lg bg-navyBlue bg-opacity-80 border-4 border-double">
+          <button className="py-2 px-4 rounded-lg bg-navyBlue border-4 border-double hover:bg-white hover:bg-opacity-40 hover:text-black">
             Send a Valentine's Card
           </button>
         </div>
