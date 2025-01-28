27532d73b7aa511021ce0c142c925ec4a392414b
Laura <laura.e.vollmer@gmail.com>
2025-01-28 12:24:02 -0600

    creating a gradient colored header

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -6,13 +6,15 @@ import CardPink from "../assets/combinedcardpink-removebg-preview.png";
 
 const LandingPage = () => {
   return (
-    <div className="bg-bubblePink w-screen h-screen flex flex-col justify-between items-center p-10">
+    <div className="bg-redred w-screen h-screen flex flex-col justify-between items-center p-10">
       <div>
         <Navigation />
       </div>
       <div className="flex grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="font-rubik text-white flex flex-col items-center justify-center">
-          <h1 className="text-4xl">Send a beautiful Valentine's Digital Card</h1>
+          <h1 className="text-4xl bg-gradient-to-r from-blue-600 to-blue-800 inline-block font-bold text-transparent bg-clip-text">
+            Send a beautiful Valentine's Digital Card
+          </h1>
           <p className="text-md text-center">
             Looking for the perfect way to show your love this Valentine's Day?
             Send a personalized digital card that speaks from the heart! Our
@@ -22,7 +24,9 @@ const LandingPage = () => {
             your message unforgettable. Quick, easy, and delivered instantly –
             send a little love today!
           </p>
-          <button className="py-2 px-4 rounded-lg bg-navyBlue bg-opacity-80 border-4 border-double">Send a Valentine's Card</button>
+          <button className="py-2 px-4 rounded-lg bg-navyBlue bg-opacity-80 border-4 border-double">
+            Send a Valentine's Card
+          </button>
         </div>
         <div className="relative flex flex-col items-center justify-center">
           <img src={CardPink} alt="pink card" className="h-96 w-96" />
