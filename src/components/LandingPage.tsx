f305bf1ecbac4671de19ee657b5d9dd16bc31880
Laura <laura.e.vollmer@gmail.com>
2025-01-29 09:21:54 -0600

    updating color scheme

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -9,13 +9,13 @@ import { FaArrowDown } from "react-icons/fa";
 
 const LandingPage = () => {
   return (
-    <div className="bg-bubblePink w-screen h-screen">
+    <div className="bg-rojo w-screen h-screen">
       <div className="flex flex-start items-start space-y-4 p-10">
         <img src={HeartPost} alt="heart post" className="h-16 w-16" />
       </div>
-      <div className="bg-bubblePink p-10 flex grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="font-rubik p-10 text-white flex flex-col items-center justify-center space-y-6">
-          <h1 className="text-4xl bg-gradient-to-r from-navyBlue to-blue-800 inline-block font-bold text-transparent bg-clip-text">
+      <div className="bg-rojo p-10 flex grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="font-rubik p-10 text-black flex flex-col items-center justify-center space-y-6">
+          <h1 className="text-4xl bg-gradient-to-r from-salmon_pink to-pink-100 inline-block font-bold text-transparent bg-clip-text">
             It's never been easier to send a Valentine's Day card
           </h1>
           <p className="text-md text-left">
@@ -27,18 +27,18 @@ const LandingPage = () => {
             your message unforgettable. Quick, easy, and delivered instantly –
             send a little love today!
           </p>
-          <button className="flex flex-row items-center justify-center space-x-2 py-2 px-4 rounded-lg bg-navyBlue border-4 border-double hover:bg-white hover:bg-opacity-40 hover:text-black">
+          <button className="text-black flex flex-row items-center justify-center space-x-2 py-2 px-4 rounded-lg bg-pale_dogwood border-4 border-double hover:bg-salmon_pink hover:bg-opacity-40 hover:text-black">
             <FaArrowDown />
             <p>Send a Valentine's Card</p>
           </button>
         </div>
-        <div className="relative bg-bubblePink flex flex-col items-center justify-center">
+        <div className="relative bg-rojo flex flex-col items-center justify-center">
           <img src={BackgroundCard} alt="photo" className="rounded-lg"/>
         </div>
       </div>
-      <div className="bg-bubblePink flex flex-col items-center justify-center space-y-4 p-10">
+      <div className="bg-rojo flex flex-col items-center justify-center space-y-4 p-10">
         <div className="flex flex-col items-center justify-center space-y-4">
-          <h1 className="text-2xl text-navyBlue font-bold">
+          <h1 className="text-2xl text-pale_dogwood font-bold">
             Why Choose Our Valentine's Digital Cards?
           </h1>
         </div>
