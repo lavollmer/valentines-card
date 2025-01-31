0b0db365ed1a135f1df79dbbd57a652ba9592ac7
Laura <laura.e.vollmer@gmail.com>
2025-01-31 10:35:55 -0600

    Added finalzed touches to style on countdown clock - border, bg color, font

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -13,11 +13,11 @@ const Countdown = () => {
   const secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
 
   return (
-    <div className="flex flex-col items-center justify-center text-white">
+    <div className="flex flex-col space-y-10 items-center justify-center text-white">
       <div>
         <h2 className="text-4xl font-bold">Countdown to Valentine's Day</h2>
       </div>
-      <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+      <div className="grid grid-flow-col gap-5 text-center auto-cols-max bg-black p-6 rounded-lg">
         <div className="flex flex-row space-x-6 font-rubik">
           <div className="flex flex-col">
             <p className="text-4xl">{daysLeft}</p>
@@ -26,17 +26,17 @@ const Countdown = () => {
           <p className="text-4xl">:</p>
           <div className="flex flex-col">
             <p className="text-4xl">{hoursLeft}</p>
-            <p>days</p>
+            <p>hours</p>
           </div>
           <p className="text-4xl">:</p>
           <div className="flex flex-col">
             <p className="text-4xl">{minutesLeft}</p>
-            <p>days</p>
+            <p>minutes</p>
           </div>
           <p className="text-4xl">:</p>
           <div className="flex flex-col">
             <p className="text-4xl">{secondsLeft}</p>
-            <p>days</p>
+            <p>seconds</p>
           </div>
         </div>
       </div>
