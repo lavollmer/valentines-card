4ca126eaa8fe9bb5640a6b21f942dcebbed3e72e
Laura <laura.e.vollmer@gmail.com>
2025-02-04 13:12:04 -0600

    Working on the Sent Card component and adding a Navigation component

--- src/components/SentCard.tsx
+++ src/components/SentCard.tsx
@@ -2,6 +2,7 @@ import YouAreLoved from "./YouAreLoved";
 import Vintage from "./Vintage";
 import ValentinesCard from "./ValentinesCard";
 import { useLocation } from "react-router-dom";
+import Navigation from "./Navigation";
 
 interface SentCardProps {
   selectedCard: string;
@@ -22,41 +23,46 @@ const SentCard: React.FC<SentCardProps> = () => {
   console.log(selectedCard);
 
   return (
-    <div className="flex flex-col h-screen w-screen bg-mauvePink justify-center items-center font-rubik space-y-10">
-      <h1 className="font-xl text-white">Your Card Has Been Sent!</h1>
+    <>
       <div>
-        <button
-          type="button"
-          onClick={returnHome}
-          className="px-8 py-2 bg-eggplant hover:bg-greenCadet text-white rounded-lg font-rubik font-bold"
-        >
-          Send Another Card
-        </button>
+        <Navigation />
       </div>
-      <div>
-        {selectedCard === "YouAreLoved" && (
-          <YouAreLoved
-            firstName={firstName}
-            message={message}
-            fromName={fromName}
-          />
-        )}
-        {selectedCard === "Vintage" && (
-          <Vintage
-            firstName={firstName}
-            message={message}
-            fromName={fromName}
-          />
-        )}
-        {selectedCard === "Valentines" && (
-          <ValentinesCard
-            firstName={firstName}
-            message={message}
-            fromName={fromName}
-          />
-        )}
+      <div className="flex flex-col h-screen w-screen bg-mauvePink justify-center items-center font-rubik space-y-10 bg-gradient-to-r from-red-300 to-pink-600">
+        <h1 className="font-xl text-white">Your Card Has Been Sent!</h1>
+        <div>
+          <button
+            type="button"
+            onClick={returnHome}
+            className="px-8 py-2 bg-eggplant hover:bg-greenCadet text-white rounded-lg font-rubik font-bold"
+          >
+            Send Another Card
+          </button>
+        </div>
+        <div>
+          {selectedCard === "YouAreLoved" && (
+            <YouAreLoved
+              firstName={firstName}
+              message={message}
+              fromName={fromName}
+            />
+          )}
+          {selectedCard === "Vintage" && (
+            <Vintage
+              firstName={firstName}
+              message={message}
+              fromName={fromName}
+            />
+          )}
+          {selectedCard === "Valentines" && (
+            <ValentinesCard
+              firstName={firstName}
+              message={message}
+              fromName={fromName}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
