ba74991e5f2166927c391e543e56ab71667e6231
Laura <laura.e.vollmer@gmail.com>
2025-02-04 13:01:27 -0600

    Formatting the FAQ page

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,8 +1,9 @@
 //importing CSS files and images into React component
 import { useState } from "react";
-import iconStar from "./assets/icon-star.svg";
-import { FaMinus } from "react-icons/fa";
-import { FaPlus } from "react-icons/fa";
+import PlusIcon from "../assets/plus.svg";
+import XIcon from "../assets/cancel.svg";
+import { CiHeart } from "react-icons/ci";
+import Navigation from "./Navigation";
 
 function FAQs() {
   //initializes a state variable named "active" and a function to update it as "setActive" using the useState hook, intial value is set to false
@@ -35,47 +36,48 @@ function FAQs() {
   //returns the JSX structure of the component
   return (
     <>
-      <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-light-pink font-worksans">
-        <div className="w-[400px] md:max-w-[600px] md:w-auto z-10 rounded-lg flex flex-col p-3 text-dark-purple bg-white">
-          <div className="flex items-center my-4">
-            {/* <img
-              src={iconStar}
-              alt="avatar"
-              className="rounded-full h-20 w-20"
-            /> */}
-            <h1 className="font-bold text-dark-purple text-3xl md:text-4xl">
-              FAQS
-            </h1>
-          </div>
-          <div className="flex flex-col gap-4">
-            {/* iterates over each item in the data array - for each item the function returns JSX */}
-            {data.map((item) => (
-              // key prop is required for each child in a list
-              <div key={item.id}>
-                <div className="flex justify-between p-2 hover:text-purple-600 items-center">
-                  <h1 className="text-lg font-bold w-[75%] md:w-[90%] leading-6 tracking-wide mb-2 cursor-pointer">
-                    {item.intro}
-                  </h1>
-                  {/* image to serve as button to toggle active state */}
-                  <img
-                    src={active === item.id ? <FaMinus /> : <FaPlus />}
-                    alt="icon"
-                    className="w-6 h-6 cursor-pointer"
-                    // onClick event handler to toggle active state
-                    onClick={() =>
-                      active === item.id ? setActive(0) : setActive(item.id)
-                    }
-                  />
+      <div className="bg-gradient-to-r from-red-300 to-pink-600">
+        <Navigation />
+        <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-light-pink font-worksans">
+          <div className="w-[400px] md:max-w-[800px] md:w-auto z-10 rounded-lg flex flex-col p-3 text-dark-purple bg-white">
+            <div className="flex items-center my-4">
+              {/* <CiHeart className="h-4 w-4" /> */}
+              <h1 className="font-bold text-dark-purple text-3xl md:text-4xl">
+                HeartPost FAQS
+              </h1>
+            </div>
+            <div className="flex flex-col gap-4">
+              {/* iterates over each item in the data array - for each item the function returns JSX */}
+              {data.map((item) => (
+                // key prop is required for each child in a list
+                <div key={item.id}>
+                  <div className="flex justify-between p-2 hover:text-purple-600 items-center">
+                    <h1 className="text-lg font-bold w-[75%] md:w-[90%] leading-6 tracking-wide mb-2 cursor-pointer">
+                      {item.intro}
+                    </h1>
+                    {/* image to serve as button to toggle active state */}
+                    <img
+                      src={active === item.id ? XIcon : PlusIcon}
+                      alt="icon"
+                      className="w-6 h-6 cursor-pointer"
+                      // onClick event handler to toggle active state
+                      onClick={() =>
+                        active === item.id ? setActive(0) : setActive(item.id)
+                      }
+                    />
+                  </div>
+                  {/* conditional rendering block that checks if the current item is the active one */}
+                  {active === item.id ? (
+                    <p className="text-gray-500 dark:text-gray-400">
+                      {item.pg}
+                    </p>
+                  ) : (
+                    <p></p>
+                  )}
+                  <hr />
                 </div>
-                {/* conditional rendering block that checks if the current item is the active one */}
-                {active === item.id ? (
-                  <p className="text-gray-500 dark:text-gray-400">{item.pg}</p>
-                ) : (
-                  <p></p>
-                )}
-                <hr />
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
