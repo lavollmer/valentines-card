9be057265ee8ee090e27cd6dd0fd0df21e974f32
Laura <laura.e.vollmer@gmail.com>
2025-02-04 13:03:24 -0600

    Added Navigation on HeartPost Navigation logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,10 +14,16 @@ const Navigation = () => {
     e.preventDefault();
     navigate("/previewcards");
   };
+
+  const handleHomeClick = (e:React.FormEvent) => {
+    e.preventDefault();
+    navigate("/");
+  }
+
   return (
     <div className="fixed top-0 w-full">
       <nav className="flex flex-row justify-between w-full bg-gradient-to-r from-red-300 to-pink-600 pl-10 pr-10 p-10">
-        <h1 className="text-4xl text-white font-rubik font-bold"> HeartPost</h1>
+        <h1 onClick={handleHomeClick} className="text-4xl text-white font-rubik font-bold cursor-pointer">HeartPost</h1>
         <div className="flex flex-row space-x-4 items-center justify-center">
           <div onClick={handleFAQClick}>
             <ButtonVal buttonTitle="FAQs" />
