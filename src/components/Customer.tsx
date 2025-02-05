fdeb09893e94f18ebeac09cd014d737b386f4a1a
Laura <laura.e.vollmer@gmail.com>
2025-02-05 13:01:04 -0600

    Updating the Customer information

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -2,14 +2,14 @@ import { FaStar } from "react-icons/fa6";
 
 const Customer = () => {
   return (
-    <div className="flex flex-col font-rubik">
+    <div className="flex flex-col space-y-10 font-rubik p-20">
       <div className="flex flex-row items-center">
         <h1 className="mt-8 mb-6 text-2xl lg:text-3xl font-bold text-gray-100">
           What They've Said
         </h1>
       </div>
-      <div className="flex flex-row items-center justify-between space-y-10">
-        <div className="flex flex-col">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-col space-y-10">
           <div className="flex flex-row items-center justify-center space-x-2">
             <FaStar color="yellow" />
             <FaStar color="yellow" />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,36 +4,32 @@ const Footer = () => {
   return (
     <div className="flex flex-col space-y-10 md:space-x-4 text-center md:text-left md:flex-row justify-center md:justify-evenly bg-black pt-20 pb-20 font-rubik">
       <div className="flex flex-row justify-center md:flex-col md:m-6">
-        <h1 className="text-white">HeartPost</h1>
+        <h1 className="text-white text-4xl">HeartPost</h1>
       </div>
       <div className="flex flex-col space-y-6 text-sm">
         <div>
           <h1 className="font-bold text-white">Product</h1>
         </div>
-        <div className="flex flex-col left-0 space-y-2 text-white font-semilight">
+        <div className="flex flex-col left-0 space-y-2 text-white">
           <p>Overview</p>
           <p>Pricing</p>
-          <p>Marketplace</p>
-          <p>Features</p>
-          <p>Integrations</p>
         </div>
       </div>
       <div className="flex flex-col space-y-6 text-sm">
         <div>
           <h1 className="font-bold text-white">Company</h1>
         </div>
-        <div className="flex flex-col left-0 space-y-2 text-white font-semilight">
+        <div className="flex flex-col left-0 space-y-2 text-white">
           <p>About</p>
           <p>Team</p>
           <p>Blog</p>
-          <p>Careers</p>
         </div>
       </div>
       <div className="flex flex-col space-y-6 text-sm">
         <div>
           <h1 className="font-bold text-white">Connect</h1>
         </div>
-        <div className="flex flex-col left-0 space-y-2 text-white font-semilight">
+        <div className="flex flex-col left-0 space-y-2 text-white">
           <p>Contact</p>
           <p>Newsletter</p>
           <p>LinkedIn</p>
